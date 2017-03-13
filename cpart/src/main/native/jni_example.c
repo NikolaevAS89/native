f3190665f8eb190484751e4b125e4758fb692b76
NikolaevAS89 <t.i.m.e.s.t.o.p@mail.ru>
2017-03-13 07:40:51 +0300

    Delete trush

--- cpart/src/main/native/jni_example.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <jni.h>
-#include <nativeStuff.h>
-
-JNIEXPORT jstring JNICALL Java_timestop_Main_get(JNIEnv *, jobject) {
- return "Hi from C++";
-};
